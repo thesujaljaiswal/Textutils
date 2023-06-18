0600306526808cac4f6c68bb336040fbe34dddca
anjaanid <sujal676865@ascs.sies.edu.in>
2023-06-18 16:34:48 +0530

    commit from sujal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+// import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
+import Gender_Store from './Components/Gender_Store';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Gender_Store/>
       <Footer/>
     </>
   );
