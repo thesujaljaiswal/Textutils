f2e435ace5c016f52b89a0df8bf265264ac55c22
Mohnish <mohnishkalaimani@gmail.com>
2023-06-19 12:41:08 +0530

    Mohnish Commits

---
+++ src/Components/Bestseller.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Imgdes from './Imgdes'
+
+export default function Bestseller() {
+  return (
+    <>
+      <div className="container">
+        <h1>Bestsellers</h1>
+    </div>
+      <div className="carousel-cards">
+        <Imgdes/>
+        <Imgdes />
+        <Imgdes />
+      </div>
+    </>
+  )
+}
---
+++ src/Components/Imgdes.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./imgdes.css";
+
+export default function Imgdes() {
+  return (
+    <>
+    <div className="outer-container">
+      <div className="container">
+        <div className="image">
+          <img
+            className="w-full"
+            id="img"
+            src={require("./female.jpeg")}
+            alt="female"
+          />
+        </div>
+        <div className="des">Coco Stripes Heavyweight Oversized T-Shirt</div>
+        <div className="pricing">
+          <div>
+            <p>
+              <s>₹899.00</s>
+            </p>
+          </div>
+
+          <div>
+            <p className="price">₹599.00</p>
+          </div>
+
+        </div>
+
+
+        <div className="wishlist-buttons">
+          <div className="opt">
+            <a href="/">SELECT OPTIONS</a>
+          </div>
+          <div className="wishlist">
+            <a href="/">
+              <img
+                className="w-full"
+                id="wl"
+                src={require("./heart.png")}
+                alt="heart"
+                height={20}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </>
+  );
+}
