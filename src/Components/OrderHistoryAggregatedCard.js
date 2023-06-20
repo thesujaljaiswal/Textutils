0cdc16b6e0f03feafa51f1a47c85e93d972e2036
Paddy <padmanabhanpaddy10@gmail.com>
2023-06-20 22:02:41 +0530

    ProductPage, WishlistPage, OrderHistoryPage & ProductListingPage and its components added(paddy)

---
+++ src/Components/OrderHistoryAggregatedCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './OrderHistoryAggregated.css'
+
+export default function OrderHistoryAggregatedCard() {
+  return (
+    <div>
+        {/* The outer main component holding details of aggregated order */}
+        <div className="outer-heading">
+                <div><p>Order Placed: 27-April-2003</p></div>
+                <div><p>Total: 86,000</p></div>
+                <div><p>Shipped to: Elon Musk</p></div>
+                <div><p>Order ID: #123-456-789</p></div>
+                <div><p>Order Status: DELIVERED</p></div>
+            </div>
+    </div>
+  )
+}
